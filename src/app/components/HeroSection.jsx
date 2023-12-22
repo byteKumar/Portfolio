2a7a6892b869f22312f4e753d4fa53a7c5a0dfe1
KavyaRachana <kavya.kundana5@gmail.com>
2023-12-22 11:43:32 -0500

    minor fix

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import heroImage from "../../../public/hero-image.png"
 
 const HeroSection = () => {
   return (
@@ -65,7 +66,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.png"
+              src={heroImage}
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
