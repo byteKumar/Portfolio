1a673e8a1a1f0bd823442cd618a411d7a10532fd
KavyaRachana <kavya.kundana5@gmail.com>
2023-12-22 22:16:52 -0500

    minor changes

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -17,17 +17,28 @@ const TAB_DATA = [
       <li>Java</li>
       <li>Python</li>
       <li>Oracle</li>
-      <li>HTML</li>
+      <li>SQL</li>
     </div>
   </ul>
 
   <ul className="list-disc pl-2">
     <div>
-      <li>Node.js</li>
-      <li>Express</li>
-      <li>PostgreSQL</li>
       <li>MySQL</li>
+      <li>PostgreSQL</li>
+      <li>MongoDB</li>
+      <li>Oracle</li>
+      <li>Hadoop</li>
+      <li>HTML</li> 
+    </div>
+  </ul>
+
+  <ul className="list-disc pl-2">
+    <div>
+      <li>CSS</li>
+      <li>TypeScript</li>
       <li>JavaScript</li>
+      <li>Express</li>
+      <li>Node.js</li>
       <li>React</li> 
     </div>
   </ul>
@@ -44,12 +55,16 @@ const TAB_DATA = [
           <p> September 2023 - May 2025 </p>
           <p>Master of Science in Software Engineering Systems</p>
           <p>GPA: 3.85/4</p>
+          <p>Revelant Courses: Object Oriented Design with JAVA, Web Design and User Experience, Database Management and Database Design, Network Structures and Cloud Computing
+</p>
         </li>
         <br />
         <li><b>Dadi Insitute of Engineering and Technology </b>
             <p>May 2015 - June 2019</p>
             <p>Bachelor of Technology in Computer Science & Engineering</p>
             <p>GPA: 8.2/10</p>
+            <p>Relevant Courses: Computer Programming, OOP with C++,  Data Structures, Concepts OF JAVA, Web Technologies, Learning Advanced JAVA, Software Engineering, DBMS
+</p>
         </li>
       </ul>
     ),
@@ -62,11 +77,15 @@ const TAB_DATA = [
         <li>Technology Analsyt 
           <p>December 2019 - July 2023</p>
           <p><i>Infosys Ltd., Hyderabad, India</i></p>
+          <p>- Led a team that played a pivotal role in supporting the production of one of National Australia Bank&apos;s (NAB) largest data warehouses. Managed the execution of numerous automated scripts on a daily basis, each containing intricate business logic. Oversaw the entire lifecycle of data processes, including ingestion, engineering, and distribution, catering to 75% of the bank&apos;s global customer base.</p>
+          <p>- Engaged directly with clients to grasp their needs and requirements, advocating for them in the field to ensure project alignment. Developed tailored SQL scripts for data ingestion, addressing compatibility issues with the SQL metadata script for 80% of the received data. Collaborated closely with the QA team to guarantee a smooth transition of accurately delivered data for all banking processes.</p>
+          <p>- Provided comprehensive onboarding guidance for both onsite and offshore teams, conducting knowledge transfer trainings to enhance team capabilities.</p>
         </li>
         <br />
         <li>Software Developer Intern
         <p>December 2019 - July 2023</p>
           <p><i>Global InfoTech, Hyderabad, India</i></p>
+          <p>- Established a database that encompasses authentic ratings for 60% of the applications on the Google Play Store. Developed predictive software using JAVA to optimize the efficiency of various solutions, and actively contributed to the creation of both the front end and back end of the application.</p>
         </li>
       </ul>
     ),
